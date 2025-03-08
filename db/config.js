95fbb91444dce0fabbf439e1a68ba79bcb38248f
NontasBak <nontasbak@gmail.com>
2025-03-08 17:57:35 +0200

    Initialize database tables

---
+++ db/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const localConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+}
+
+const dbConfig = localConfig;
+
+module.exports = dbConfig;
---
+++ db/populate.js
@@ -0,0 +1,37 @@
+const pool = require("./pool");
+
+async function createTables() {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        first_name VARCHAR(255),
+        last_name VARCHAR(255),
+        email VARCHAR(255),
+        password VARCHAR(255)
+        );
+
+        CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        user_id INTEGER REFERENCES users ( id ),
+        title VARCHAR(255),
+        text TEXT,
+        date DATE
+        );
+    `;
+
+    try {
+        await pool.query(sql);
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function main() {
+    try {
+        await createTables();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
